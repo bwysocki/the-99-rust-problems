b733ffab0321b758a4dcacc46c9507b6a2420af2
Bartosz Wysocki <ext-bartosz.wysocki@maas.global>
2022-12-20 08:00:34 +0100

    The 2 and 3 problems

--- the_02_find_the_last_but_one_box_of_a_list/target/debug/deps/the_02_find_the_last_but_one_box_of_a_list-45a4fdca276364ad.d
+++
@@ -1,5 +0,0 @@
-/home/stalos/Projects/the-99-rust-problems/the_02_find_the_last_but_one_box_of_a_list/target/debug/deps/the_02_find_the_last_but_one_box_of_a_list-45a4fdca276364ad: src/lib.rs
-
-/home/stalos/Projects/the-99-rust-problems/the_02_find_the_last_but_one_box_of_a_list/target/debug/deps/the_02_find_the_last_but_one_box_of_a_list-45a4fdca276364ad.d: src/lib.rs
-
-src/lib.rs:
--- the_03_find_the_Kth_element_of_a_list/target/debug/deps/the_03_find_the_Kth_element_of_a_list-df39bb9e1b7e0266.d
+++
@@ -1,5 +0,0 @@
-/home/stalos/Projects/the-99-rust-problems/the_03_find_the_Kth_element_of_a_list/target/debug/deps/the_03_find_the_Kth_element_of_a_list-df39bb9e1b7e0266: src/lib.rs
-
-/home/stalos/Projects/the-99-rust-problems/the_03_find_the_Kth_element_of_a_list/target/debug/deps/the_03_find_the_Kth_element_of_a_list-df39bb9e1b7e0266.d: src/lib.rs
-
-src/lib.rs:
